b086343025353e21b372ff90519dbcee691f97c5
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-12-31 16:12:49 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,79 @@
+class Dog
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(id = nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = "DROP TABLE dogs"
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        if self.id
+            self.update
+        else
+            sql = <<-SQL
+            INSERT INTO dogs(name, breed) VALUES (?, ?)
+            SQL
+            DB[:conn].execute(sql, name, breed)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
+        self
+    end
+
+    def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+        dog
+    end
+
+    def self.find_by_id(id)
+        sql = "SELECT * FROM dogs WHERE id = ?"
+        result = DB[:conn].execute(sql, id)[0]
+        new_from_db(result)
+    end
+
+    def self.find_or_create_by(name:, breed:)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+        if !dog.empty?
+            dog_data = dog[0]
+            dog = Dog.new(dog_data[0])
+        else
+            dog = self.create(name: name, breed: breed)
+        end
+        dog
+    end
+
+    def self.new_from_db(row)
+        new_dog = self.new(id: id, name: name, breed:breed)
+        new_dog.id = row[0]
+        new_dog.name = row[1]
+        new_dog.breed = row[2]
+        new_dog
+    end
+
+    def self.find_by_name
+    end
+
+    def update
+        sql = "UPDATE dogs SET name =?, breed =? WHERE name =?"
+        DB[:conn].execute(sql, self.name, self.breed)
+    end
+    end
+end
