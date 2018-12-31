339e6d862eae96a43c563b1bc5009ee98278ff89
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-12-31 16:20:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ class Dog
     end
 
     def update
-        sql = "UPDATE dogs SET name =?, breed =? WHERE name =?"
-        DB[:conn].execute(sql, self.name, self.breed)
+        sql = "UPDATE dogs SET name =?, breed =? WHERE id =?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
     end
 end
